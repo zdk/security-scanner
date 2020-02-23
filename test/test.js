d74d55344ad37aeb37e1513b5f3b032cc0a0e15c
zdk <di.warachet@gmail.com>
2020-02-23 21:08:53 +0700

    [RED] Add a test for submitting a scan result

--- test/test.js
+++ test/test.js
@@ -6,25 +6,43 @@ const chaiHttp = require('chai-http');
 const should = chai.should();
 
 const app = require('../app');
-const { Result } = require('../models');
+const {
+    Result
+} = require('../models');
 
 chai.use(chaiHttp);
 
 describe('Result API', () => {
-  beforeEach((done) => {
-    Result.destroy({
-      where: {},
-      truncate: true,
-    });
-    done();
-  });
-  describe('GET /', () => {
-    it('Getting all results', (done) => {
-      chai.request(app).get('/').end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a('array');
+
+    beforeEach((done) => {
+        Result.destroy({
+            where: {},
+            truncate: true,
+        });
         done();
-      });
     });
-  });
+
+    describe('GET /', () => {
+        it('Getting all results', (done) => {
+            chai.request(app).get('/').end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                done();
+            });
+        });
+    });
+
+    describe('POST /', function() {
+        it('Submit a new scan', function(done) {
+            var req  = {
+                repositoryName: 'https://github.com/nodejs/node',
+            }
+            chai.request(app).post('/').send(result).end(function(err, res) {
+                console.log(res);
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                done();
+            });
+        });
+    });
 });
